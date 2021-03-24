fec469603fcac2b45d48f6be48ac4faa14db5ef1
Fathima7391 <75068068+Fathima7391@users.noreply.github.com>
2021-03-24 20:46:13 +0530

    Add files via upload

---
+++ graphics/circle.py
@@ -0,0 +1,5 @@
+from math import pi
+def area_circle(r):
+	return(pi*r*r)
+def perimeter_circle(r):
+	return(2*pi*r)
---
+++ graphics/threeD/cuboid.py
@@ -0,0 +1,4 @@
+def area(l,b,h):
+	return(l*b*h)
+def perimeter(l,b,h):
+	return(4*(l+h+h))
